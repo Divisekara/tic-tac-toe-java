ca0bcaada7eb7b9d5d5c0a652fce7baa17fad7c5
ASITHA INDRAJITH DIVISEKARA <46625806+Divisekara@users.noreply.github.com>
2021-12-27 03:13:27 +0530

    winner selecting algorithm

--- tic_tac_toe.java
+++ tic_tac_toe.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 public class tic_tac_toe {
 
     public static void main(String[] args) {
@@ -11,7 +13,24 @@ public class tic_tac_toe {
         };
 
         gameIntro();
-        printBoard(board);
+
+        Scanner scanner = new Scanner(System.in);
+
+        for (int i = 0; i < 9; i++) {
+            int user;
+            if (i % 2 == 0) {
+                user = 1;
+            } else {
+                user = 2;
+            }
+            System.out.println("-------User One-----");
+            System.out.println("Here is the current board - ");
+            printBoard(board);
+
+            System.out.println("Enter a number between 1-9 not already occupied - ");
+            String userInput = scanner.nextLine();
+            updateBoard(board, user, userInput);
+        }
 
     }
 
@@ -30,7 +49,7 @@ public class tic_tac_toe {
         };
         printBoard(boardConvention);
 
-        System.out.println("No more intro. Lets start the game!.");
+        System.out.println("No more intro. Lets start the game!. \n\n");
     }
 
     public static void printBoard(char[][] board) {
@@ -39,4 +58,48 @@ public class tic_tac_toe {
         }
     }
 
+    public static void updateBoard(char[][] board, int user, String userInput) {
+        int input = Integer.parseInt(userInput);
+        int row, column;
+        row = ((input - 1) / 3) * 2;
+        column = ((input - 1) % 3) * 2;
+
+        if (user == 1) {
+            board[row][column] = (char) ('X' + '0');
+        } else {
+            board[row][column] = (char) ('O' + '0');
+        }
+    }
+
+    public static void winner(char[][] board) {
+        String[][] boardNumbers = new String[3][3];
+
+        for (int j = 1; j < 10; j++) {
+            int row, column;
+            row = (j / 3) * 2;
+            column = (j % 3) * 2;
+            boardNumbers[row][column] = String.valueOf(board[row][column]);
+        }
+    }
+
+    public static String checkPattern(String[] pattern) {
+        int number_X = 0;
+        int number_O = 0;
+        for (String i : pattern) {
+            if (i == "X") {
+                number_X += 1;
+            } else {
+                number_O += 1;
+            }
+        }
+
+        if (number_O == 3) {
+            return "X";
+        } else if (number_X == 3) {
+            return "O";
+        } else {
+            return "play";
+        }
+
+    }
 }
